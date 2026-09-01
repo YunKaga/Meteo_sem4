ebe80a54c9d80989f771f420370f910f38dcbd2a
YunKage <voronzowyura@gmail.com>
2026-09-01 13:27:02 +0700

    -мусор

--- Gui/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_weather_client_FILE /mnt/disk2/prog/sem4/ProjectX/Gui/build/weather_client)
-set(__QT_DEPLOY_TARGET_weather_client_TYPE EXECUTABLE)
--- Gui/build/CMakeFiles/weather_client_autogen_timestamp_deps.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for weather_client_autogen_timestamp_deps.
